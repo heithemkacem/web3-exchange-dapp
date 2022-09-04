751ef11d75e02535d388705d49219f2cfa0825c0
Yassine Zaanouni <yassinezaanouni@gmail.com>
2022-09-04 10:29:19 +0100

    Added tailwind

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,17 +1,12 @@
-import {
-  createClient,
-  configureChains,
-  defaultChains,
-  WagmiConfig,
-} from "wagmi";
+import "../styles/globals.css";
+
+import { createClient, configureChains, defaultChains, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { SessionProvider } from "next-auth/react";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 
-const { provider, webSocketProvider, chains } = configureChains(defaultChains, [
-  publicProvider(),
-]);
+const { provider, webSocketProvider, chains } = configureChains(defaultChains, [publicProvider()]);
 
 const { connectors } = getDefaultWallets({
   appName: "My RainbowKit App",
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function HomePage() {
-    return <div>Welcome to Next.js!</div>;
+  return <div className="text-red-400">Welcome to Next.js!</div>;
 }
 
 export default HomePage;
