76a71f1ab30781ae11ab3d692e3e2c487e616578
Heithem Kacem <heythem.kacem@whitecapetech.com>
2022-09-04 09:35:43 +0100

    rainbowkit and moralis web3 auth

---
+++ pages/_app.jsx
@@ -0,0 +1,42 @@
+import {
+  createClient,
+  configureChains,
+  defaultChains,
+  WagmiConfig,
+} from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { SessionProvider } from "next-auth/react";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
+
+const { provider, webSocketProvider, chains } = configureChains(defaultChains, [
+  publicProvider(),
+]);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+  autoConnect: true,
+  // added connectors from rainbowkit
+  connectors,
+});
+
+// added RainbowKitProvider wrapper
+function MyApp({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={client}>
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </SessionProvider>
+    </WagmiConfig>
+  );
+}
+
+export default MyApp;
